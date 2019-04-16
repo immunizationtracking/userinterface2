996290ba348f6ea22cde8d611acd1d451abe55fe
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-04-16 12:01:30 -0500

    vac scheudle layout

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ class Menu {
     console.log(this.attrs)
     this.attrs.addEventListener('mouseover', () => this.changeColor());
     this.attrs.addEventListener('mouseleave', () => this.changeBack());
-    this.attrs.addEventListener('click', () => this.showInfo());
+    this.attrs.addEventListener('click', () => new Card(this.attrs));
+  }
   changeColor() {
-    this.attrs.classList.remove('oneItem')
-    this.attrs.classList.add('oneItemColorChange')
+    this.attrs.classList.remove('oneItem');
+    this.attrs.classList.add('oneItemColorChange');
+
   }
   changeBack() {
-    this.attrs.classList.remove('oneItemColorChange')
-    this.attrs.classList.add('oneItem')
+    this.attrs.classList.remove('oneItemColorChange');
+    this.attrs.classList.add('oneItem');
   }
-  showInfo() {
-    this.attrs.classList.remove('hidden')
+
+}
+
+class Card {
+  constructor(element) {
+    this.element = element;
+    console.log(this.element)
   }
 }
 
